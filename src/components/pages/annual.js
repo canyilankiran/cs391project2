a0d091759ca1df50d47c1d63de19848142a71876
Kıvanç Yılmaz <58771711+kivanc53@users.noreply.github.com>
2021-06-12 22:41:03 +0300

    Add files via upload

---
+++ src/components/pages/annual.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const AnnualReport = () => {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'Right',
+                alignItems: 'Right',
+                height: '100vh'
+            }}
+        >
+            <h2 align="center"> You will see the economic details</h2>
+        </div>
+);
+};
+
+export default AnnualReport;
